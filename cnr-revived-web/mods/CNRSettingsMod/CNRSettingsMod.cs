17e2f5957059c91d126a6935de6e9b386f904e59
CopsNRobbers <dev@copsnrobbers.local>
2026-04-01 23:25:49 -0500

    CNRSettingsMod 2.0.26 -- for some reason version bump didn't work first time

--- cnr-revived-web/mods/CNRSettingsMod/CNRSettingsMod.cs
+++ cnr-revived-web/mods/CNRSettingsMod/CNRSettingsMod.cs
@@ -34,7 +34,7 @@ namespace CNRSettingsMod
     public static class SettingsModEntry
     {
         private const string LogPath = "/storage/emulated/0/CNRMods/settings.log";
-        public  const string Version = "2.0.25";
+        public  const string Version = "2.0.26";
 
         public static void Load()
         {
